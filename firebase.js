0027352b63711a6aebe2b039b3415510423f6898
K3TANK <satunusa981@gmail.com>
2025-01-11 18:12:09 +0700

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCv9HZMgH2lA9wkO0KwzQOvAKbY7-pncto",
+    authDomain: "adidaeng-5e457.firebaseapp.com",
+    projectId: "adidaeng-5e457",
+    storageBucket: "adidaeng-5e457.firebasestorage.app",
+    messagingSenderId: "502921477836",
+    appId: "1:502921477836:web:6d61b30c038e5ca9ba8c69",
+    measurementId: "G-VTDBTBLTPW"
+  };
+  
+  const app = firebase.initializeApp(firebaseConfig);
+
